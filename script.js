6c1f24b93973c5e96be6ecf1fb6f5c8dbe233034
Peter-Kibet <peter.kibet@hotmail.com>
2023-05-16 13:36:56 +0300

    Fix linter errors

--- script.js
+++ script.js
@@ -1,31 +1,31 @@
-const menuButton = document.querySelector(".menu-button");
-const closeButton = document.querySelector(".close-button");
-const mobileMenu = document.querySelector(".mobile-menu");
-const mobileMenuItem = document.querySelectorAll(".mobile-menu-item");
+const menuButton = document.querySelector('.menu-button');
+const closeButton = document.querySelector('.close-button');
+const mobileMenu = document.querySelector('.mobile-menu');
+const mobileMenuItem = document.querySelectorAll('.mobile-menu-item');
 
 function toggleMobileMenu() {
-  mobileMenu.style.display = "flex";
+  mobileMenu.style.display = 'flex';
 }
 
 function closeMobileMenu() {
-  mobileMenu.style.display = "none";
+  mobileMenu.style.display = 'none';
 }
 
 function handleMobileMenuItemClick(event) {
-  const target = event.target;
-  const sectionId = target.getAttribute("href").substring(1);
+  const { target } = event;
+  const sectionId = target.getAttribute('href').substring(1);
 
   toggleMobileMenu();
 
   document.querySelector(sectionId).scrollIntoView({
-    behavior: "smooth",
-    block: "start",
+    behavior: 'smooth',
+    block: 'start',
   });
 }
 
-menuButton.addEventListener("click", toggleMobileMenu);
-closeButton.addEventListener("click", closeMobileMenu);
+menuButton.addEventListener('click', toggleMobileMenu);
+closeButton.addEventListener('click', closeMobileMenu);
 
 mobileMenuItem.forEach((item) => {
-  item.addEventListener("click", handleMobileMenuItemClick);
+  item.addEventListener('click', handleMobileMenuItemClick);
 });
