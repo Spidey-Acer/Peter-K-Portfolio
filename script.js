954baf1d18cc925d13749afd20bbb94bcf5f6a10
Peter-Kibet <peter.kibet@hotmail.com>
2023-05-18 05:40:46 +0300

    Implement form validation and error handling for the contact form
    This commit introduces form validation and error handling to the contact form. The following changes have been made:
    -Obtains the contact form element and its individual input fields (fullName and email).
    -Creates error container elements for email and name, setting their color to red.
    -Adds another error container element specifically for the form itself, assigning it the class name "form-error" and setting its color to red.
    -Implements the showError function, which displays an error message for a given input field, sets the error message text, appends the error container to the input's parent node, applies the "error" class to the input, and sets its border color to red.
    -Implements the removeError function, which removes the error message for a given input field, provided that the error container is a child of the input's parent node. It also removes the "error" class from the input and resets its border color.
    -These changes enable the display of error messages and appropriate styles for invalid inputs in the contact form, enhancing the user experience.

--- script.js
+++ script.js
@@ -1,31 +1,61 @@
-const menuButton = document.querySelector('.menu-button');
-const closeButton = document.querySelector('.close-button');
-const mobileMenu = document.querySelector('.mobile-menu');
-const mobileMenuItem = document.querySelectorAll('.mobile-menu-item');
+const menuButton = document.querySelector(".menu-button");
+const closeButton = document.querySelector(".close-button");
+const mobileMenu = document.querySelector(".mobile-menu");
+const mobileMenuItem = document.querySelectorAll(".mobile-menu-item");
 
 function toggleMobileMenu() {
-  mobileMenu.style.display = 'flex';
+  mobileMenu.style.display = "flex";
 }
 
 function closeMobileMenu() {
-  mobileMenu.style.display = 'none';
+  mobileMenu.style.display = "none";
 }
 
 function handleMobileMenuItemClick(event) {
   const { target } = event;
-  const sectionId = target.getAttribute('href').substring(1);
+  const sectionId = target.getAttribute("href").substring(1);
 
   closeMobileMenu();
 
   document.querySelector(sectionId).scrollIntoView({
-    behavior: 'smooth',
-    block: 'start',
+    behavior: "smooth",
+    block: "start",
   });
 }
 
-menuButton.addEventListener('click', toggleMobileMenu);
-closeButton.addEventListener('click', closeMobileMenu);
+menuButton.addEventListener("click", toggleMobileMenu);
+closeButton.addEventListener("click", closeMobileMenu);
 
 mobileMenuItem.forEach((item) => {
-  item.addEventListener('click', handleMobileMenuItemClick);
+  item.addEventListener("click", handleMobileMenuItemClick);
 });
+
+// Form Validation
+const form = document.getElementById("contactForm");
+const { fullName } = form.elements;
+const { email } = form.elements;
+const emailErrorContainer = document.createElement("small");
+emailErrorContainer.style.color = "red";
+const nameErrorContainer = document.createElement("small");
+nameErrorContainer.style.color = "red";
+// Create another errorContainer element for the form
+const formErrorContainer = document.createElement("small");
+formErrorContainer.className = "form-error";
+formErrorContainer.style.color = "red";
+
+// Show an error message
+function showError(input, message, errorContainer) {
+  errorContainer.innerText = message;
+  // Append the errorContainer to the input's parent node
+  input.parentNode.appendChild(errorContainer);
+  input.classList.add("error");
+  input.style.borderColor = "red";
+}
+
+// Remove the error message
+function removeError(input, errorContainer) {
+  if (errorContainer.parentNode === input.parentNode) {
+    input.parentNode.removeChild(errorContainer);
+    input.classList.remove("error");
+  }
+}
